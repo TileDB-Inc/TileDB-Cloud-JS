b8abcfcef3260d9ca8f8ae3e4089f23b711d2201
Konstantinos Sarantopoulos <sarantopouloskon@gmail.com>
2021-06-16 17:43:26 +0300

    Check in Capnp deserializer if data is ArrayBuffer

--- src/utils/capnpDeSerializer.ts
+++ src/utils/capnpDeSerializer.ts
@@ -1,7 +1,11 @@
+import { ArrayMetadata as ArrayMetadataType } from './../api';
 import { ArrayMetadata } from "../capnp/arrayMetadata.capnp";
 import * as capnp from "capnp-ts";
 
-export const capnpArrayMetadaDeSerializer = (buffer: ArrayBuffer | ArrayBufferLike) => {
+export const capnpArrayMetadaDeSerializer = (buffer: ArrayBuffer | ArrayBufferLike | ArrayMetadataType) => {
+  if (!isArrayBuffer(buffer)) {
+    throw new Error(`Data is not of type ArrayBuffer`);
+  }
   const message = new capnp.Message(buffer, false);
   const arrayMetadata = message.getRoot(ArrayMetadata);
   const entries = arrayMetadata.getEntries().map((entry) => {
@@ -18,3 +22,9 @@ export const capnpArrayMetadaDeSerializer = (buffer: ArrayBuffer | ArrayBufferLi
   return { entries };
 };
 
+const isArrayBuffer = (data: any): data is ArrayBuffer => {
+  if (data && data.byteLength && data.slice) {
+    return true;
+  }
+  return false;
+}
