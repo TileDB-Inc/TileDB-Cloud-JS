d8a129304d8140a37d68b231c42ca30e547dddd9
Konstantinos Sarantopoulos <sarantopouloskon@gmail.com>
2021-07-09 16:58:26 +0300

    Print data

--- examples/query.js
+++ examples/query.js
@@ -3,16 +3,24 @@ const fs = require("fs");
 const path = require("path");
 
 
-
 fs.readFile(path.resolve(__dirname, '../fixtures/response.raw'), (err, data) => {
     const arrayBuffer = toArrayBuffer(data);
-    console.log(arrayBuffer.byteLength); // 1224
     const result = deserializer.default(arrayBuffer);
-    // get last 36 bytes and cast them to int32
-    const resultsBuffer = arrayBuffer.slice(-36);
-    const nums = new Int32Array(resultsBuffer);
-    console.log(nums);
-    // console.log(result);
+    // get last X bytes and cast them to int32
+    const resultsBuffer = arrayBuffer.slice(-1 * result.totalFixedLengthBufferBytes);
+    resultsBuffer.byteLength;
+    let offsetBytes = 0;
+    
+    const attributeResults = result.attributeBufferHeaders.reduce((cur, attr) => {
+        const numOfBytesOfAttr = attr.fixedLenBufferSizeInBytes;
+        const offsetBytesWithCurrentDataBytes = offsetBytes + numOfBytesOfAttr;
+        const result = new Int32Array(resultsBuffer.slice(offsetBytes, offsetBytesWithCurrentDataBytes));
+        offsetBytes = offsetBytesWithCurrentDataBytes;
+        cur[attr.name] = result;
+        return cur;
+    }, {});
+
+    console.log(attributeResults);
   });
 
 
@@ -29,8 +37,8 @@ function toArrayBuffer(buf) {
 }
 
 /** fixtures/response.raw
- * Cell (2, 3) has data 3
- * Cell (2, 4) has data 2
+ * Cell (2, 3) has data 33
+ * Cell (2, 4) has data 28
  */
 
 
