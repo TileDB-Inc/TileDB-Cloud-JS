10e2b66023453000d8f6ab812a2aa260c73aedbf
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2022-07-15 15:06:33 +0300

    Fix case insensitive conflicts of files (#44)

--- lib/utils/getByteLengthOfdata.d.ts
+++
@@ -1,9 +0,0 @@
-import { Datatype } from "../v2";
-/**
- * Calculate the number of bytes of an array of numbers or strings
- * @param data Array of numbers or strings
- * @param type Datatype (e.g. UINT64, StringUcs2 etc)
- * @returns number of total bytes
- */
-declare const getByteLengthOfData: (data: number[] | string[], type: Datatype) => number;
-export default getByteLengthOfData;
