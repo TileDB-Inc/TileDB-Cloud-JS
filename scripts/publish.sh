62b7baeb9b0038dd9b9e8a2960e90553db71903e
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2022-09-14 13:45:48 +0300

    Fix publish script to get the correct tag
    Make publish script executable
    
    echo package version
    
    Don't run npm version
    
    Echo the latest git tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,18 +2,20 @@
 
 LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
 # Git tag is in the form v1.0.0, remove the prepending "v" to get just the version
-LATEST_GIT_TAG="${latestGitTag:1}"
+LATEST_GIT_TAG="${LATEST_GIT_TAG:1}"
 # Update package.json's version field
-npm version $LATEST_GIT_TAG
+npm version $LATEST_GIT_TAG --no-commit-hooks --no-git-tag-version
+
+echo "Updating package.json with version $LATEST_GIT_TAG"
 if [[ $LATEST_GIT_TAG == *"beta"* ]];
 then
 echo "Publishing beta version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --access public --tag beta
-elif [[ $GIT_TAG_VERSION == *"alpha"* ]];
+yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public --tag beta
+elif [[ $LATEST_GIT_TAG == *"alpha"* ]];
 then
 echo "Publishing alpha version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --access public --tag alpha
+yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public --tag alpha
 else
 echo "Publishing new version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --access public
+yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public
 fi
