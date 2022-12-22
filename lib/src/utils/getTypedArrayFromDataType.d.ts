5ec9f5584eeefffc9795b7c26965ffc3c27fe150
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2022-12-22 19:24:52 +0200

    Bundle with tsc and create override for tslib

--- lib/src/utils/getTypedArrayFromDataType.d.ts
+++
@@ -1,6 +0,0 @@
-import { Datatype } from "../v2";
-/**
- * Get the TypedArray of every type
- */
-declare const getTypedArrayFromDataType: (type: Datatype) => Uint16ArrayConstructor | Uint8ArrayConstructor | Float64ArrayConstructor | Int32ArrayConstructor | Int16ArrayConstructor | Int8ArrayConstructor | BigInt64ArrayConstructor | Uint32ArrayConstructor | BigUint64ArrayConstructor | Float32ArrayConstructor;
-export default getTypedArrayFromDataType;
