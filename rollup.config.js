9c585a1c8120606746185f245d8a8b85b6abf659
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2023-02-13 18:34:47 +0200

    Create different bundles for node & browsers

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    format: 'cjs',
-    file: 'lib/index.js'
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'cjs',
+      file: 'lib/main.js'
+    },
+    plugins: [typescript()]
   },
-  plugins: [typescript()]
-};
+  {
+    input: 'src/index.ts',
+    output: {
+      format: 'es',
+      file: 'lib/main.esm.js'
+    },
+    plugins: [typescript()]
+  }
+];
