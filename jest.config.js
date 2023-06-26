a31a76bd92353b7234415afd40f411837484f2f7
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2023-06-26 14:04:39 +0300

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   preset: "ts-jest/presets/js-with-babel",
   setupFiles: ["<rootDir>/setup.js"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!(capnp-ts)/)"
+  ],
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.json",
+      tsconfig: "tsconfig.jest.json",
     },
   },
 };
