a449433a95dc8ee45d9efd85a9f58ef038839674
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-09-12 14:12:47 +0300

    Update tools and dependencies

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    build: {
+        outDir: 'lib',
+        emptyOutDir: true,
+        lib: {
+            entry: 'src/index.ts',
+            formats: ['es']
+        },
+        minify: true,
+        rollupOptions: {
+            external: ['node:child_process']
+        },
+    },
+    worker: {
+        format: "es"
+    },
+    plugins: [dts({rollupTypes: true})]
+})
