84f9767a8d713fb73847ff03552d2dfd7e2c149b
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-09-17 11:29:20 +0300

    Add browser testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,31 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    projects: [
+      {
+        test: {
+          name: "Node",
+          include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)', './tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+        }
+      },
+      {
+        test: {
+          name: "Browser",
+          browser: {
+            enabled: true,
+            provider: 'playwright',
+            instances: [
+              { browser: "chromium" }
+            ],
+          },
+          include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)', './tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+          exclude: [
+            './src/utils/convertToArrayBufferIfNodeBuffer/convertToArrayBufferIfNodeBuffer.test.ts',
+            './src/utils/isNodeBuffer/isNodeBuffer.test.ts'
+          ]
+        }
+      }
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['html'],
