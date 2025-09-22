0028021bacba615f21a3d077fbbe9415b4e3255a
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-09-23 00:28:31 +0300

    Refactor data to ArrayBuffer

--- src/utils/dataToArrayBuffer/dataToArrayBuffer.ts
+++ src/utils/dataToArrayBuffer/dataToArrayBuffer.ts
@@ -1,44 +1,56 @@
-import { Datatype } from '../../v2';
+import { Datatype } from '../../v3';
 import getTypedArrayFromDataType from '../getTypedArrayFromDataType';
 
-const dataToArrayBuffer = (data: any = [], type: Datatype): ArrayBuffer => {
-  if (!data.length) {
-    return new ArrayBuffer(0);
-  }
-  const TypedArray = getTypedArrayFromDataType(type);
-
-  if (TypedArray) {
-    const typedArray = (TypedArray as Int32ArrayConstructor).from(data);
-    return typedArray.buffer;
-  } else if (type === Datatype.StringAscii || type === Datatype.Char) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    const textEncoder = new TextEncoder();
-    return textEncoder.encode(str).buffer;
-  } else if (type === Datatype.StringUtf8) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    const textEncoder = new TextEncoder();
-    return textEncoder.encode(str).buffer;
-  } else if (type === Datatype.StringUtf16) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    return utf16StrToArrayBuffer(str);
-  } else if (type === Datatype.StringUtf32) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    return utf32StrToArrayBuffer(str);
-  } else if (type === Datatype.StringUcs2) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    return utf16StrToArrayBuffer(str);
-  } else if (type === Datatype.StringUcs4) {
-    // If it's an array of CHARs join them together to a single string
-    const str = Array.isArray(data) ? data.join('') : data;
-    return utf32StrToArrayBuffer(str);
-  } else if (type === Datatype.Blob) {
+const dataToArrayBuffer = (
+  data: Array<unknown> | ArrayBuffer,
+  type: Datatype
+): ArrayBuffer => {
+  if (type === Datatype.Blob && data instanceof ArrayBuffer) {
     return data;
   }
+
+  if (Array.isArray(data) && data.length === 0) {
+    return new ArrayBuffer(0);
+  } else if (
+    (Array.isArray(data) && typeof data[0] === 'string') ||
+    typeof data === 'string'
+  ) {
+    const dataStr = typeof data === 'string' ? data : data.join('');
+
+    if (
+      type === Datatype.StringAscii ||
+      type === Datatype.Char ||
+      type === Datatype.StringUtf8
+    ) {
+      // If it's an array of CHARs join them together to a single string
+      return new TextEncoder().encode(dataStr).buffer;
+    } else if (type === Datatype.StringUtf16 || type === Datatype.StringUcs2) {
+      // If it's an array of CHARs join them together to a single string
+      return utf16StrToArrayBuffer(dataStr);
+    } else if (type === Datatype.StringUtf32 || type === Datatype.StringUcs4) {
+      // If it's an array of CHARs join them together to a single string
+      return utf32StrToArrayBuffer(dataStr);
+    }
+  } else if (Array.isArray(data)) {
+    const TypedArray = getTypedArrayFromDataType(type);
+
+    if (TypedArray) {
+      if (typeof data[0] === 'number') {
+        return (
+          TypedArray as Exclude<
+            typeof TypedArray,
+            BigInt64ArrayConstructor | BigUint64ArrayConstructor
+          >
+        ).from(data as Array<number>).buffer;
+      } else if (typeof data[0] === 'bigint') {
+        return (
+          TypedArray as BigInt64ArrayConstructor | BigUint64ArrayConstructor
+        ).from(data as Array<bigint>).buffer;
+      }
+    }
+  }
+
+  throw new Error('Unable to convert data to ArrayBuffer');
 };
 
 export default dataToArrayBuffer;
