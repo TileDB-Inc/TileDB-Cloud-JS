19c032c90e59dece73e722b9d8fe5d5cdd813771
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2026-01-12 09:33:37 +0100

    Switch to pipeline to github actions (#84)
    * Publish to npm registry
    
    * Verify theere's an NPM_TOKEN env var set before publishing a new version
    
    * Switch to github actions

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -z "${NODE_AUTH_TOKEN}" ]]; then
+    echo "NODE_AUTH_TOKEN is not set. Exiting."
+    exit 1
+fi
+
 LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
 # Git tag is in the form v1.0.0, remove the prepending "v" to get just the version
 LATEST_GIT_TAG="${LATEST_GIT_TAG:1}"
@@ -10,12 +15,12 @@ echo "Updating package.json with version $LATEST_GIT_TAG"
 if [[ $LATEST_GIT_TAG == *"beta"* ]];
 then
 echo "Publishing beta version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public --tag beta
+npm publish --access public --tag beta
 elif [[ $LATEST_GIT_TAG == *"alpha"* ]];
 then
 echo "Publishing alpha version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public --tag alpha
+npm publish --access public --tag alpha
 else
 echo "Publishing new version $LATEST_GIT_TAG to npm";
-yarn publish --new-version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks --access public
+npm publish --access public
 fi
